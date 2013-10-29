75052d5526891222154db48d4ef9d696ff86d509
Harald Hauknes <harald.hauknes@ntnu.no>
2013-10-29 03:11:37 +0100

    this is a hack that defeats much of the purpose of this module, it disables negotiate and ensures that browsers fall back on basic auth

--- ngx_http_auth_spnego_module.c
+++ ngx_http_auth_spnego_module.c
@@ -303,33 +303,6 @@ ngx_http_auth_spnego_headers(
     ngx_str_t *token,
     ngx_http_auth_spnego_loc_conf_t *alcf)
 {
-    ngx_str_t value = ngx_null_string;
-    /* only use token if authorized as there appears to be a bug in
-     * Google Chrome when parsing a 401 Negotiate with a token */
-    if (NULL == token || ctx->ret != NGX_OK) {
-        value.len = sizeof("Negotiate") - 1;
-        value.data = (u_char *) "Negotiate";
-    } else {
-        value.len = sizeof("Negotiate") + token->len; /* space accounts for \0 */
-        value.data = ngx_pcalloc(r->pool, value.len);
-        if (NULL == value.data) {
-            return NGX_ERROR;
-        }
-        ngx_snprintf(value.data, value.len, "Negotiate %V", token);
-    }
-
-    r->headers_out.www_authenticate =
-        ngx_list_push(&r->headers_out.headers);
-    if (NULL == r->headers_out.www_authenticate) {
-        return NGX_ERROR;
-    }
-
-    r->headers_out.www_authenticate->hash = 1;
-    r->headers_out.www_authenticate->key.len = sizeof("WWW-Authenticate") - 1;
-    r->headers_out.www_authenticate->key.data = (u_char *) "WWW-Authenticate";
-    r->headers_out.www_authenticate->value.len = value.len;
-    r->headers_out.www_authenticate->value.data = value.data;
-
     if (alcf->allow_basic) {
         ngx_str_t value2 = ngx_null_string;
         value2.len = sizeof("Basic realm=\"\"") - 1 + alcf->realm.len;
